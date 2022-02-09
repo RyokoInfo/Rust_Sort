9b0fb721c843fce6a4d1aeb44353a8072b7c3349
kyoko <dream.haku@gmail.com>
2022-02-09 10:49:30 +0000

    fn 完全に理解した

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let i = 6;
+    let answer = function(i);
+    println!("answer = {}",answer);
+
+}
+
+fn function(x:i32) -> i32{
+    let fx = x * x;
+    // println!("answer = {}",fx);
+    return fx;
 }
