8edc22711913df7771cca88c9e6930f904fdbb29
kyoko <dream.haku@gmail.com>
2022-02-11 09:12:03 +0000

    バブルソート、参照渡し　完全に理解した。

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 //use std::env;
-mod module_mult;
+//mod module_mult;
+mod mod_bable;
 
 fn main() {
-    let i = [2,5,7];
-    let answer = module_mult::function(i[0]);
-    println!("answer = {}",answer);
+    let mut array = vec![2,3,8,7,99,62,0,5,5,32];
+    //let mut i = 8;
+   
+    //ここで所有権が移動する
+    mod_bable::bable_sort(&mut array);
+
+    //表示
+    for elem in &array[..] {
+        println!("answer = {}",*elem );
+    }
+    //println!("answer i = {}",i);
 
 }
